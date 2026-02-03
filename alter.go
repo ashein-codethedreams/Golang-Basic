6a1b3632c82cc9964c3137da24cc0c4317c36d41
ashein-codethedreams <heroglasses155@gmail.com>
2026-02-03 15:46:53 +0630

    package scope

---
+++ alter.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var points = []int{23, 45, 42, 345}
+
+func sayHi(n string) {
+	fmt.Println("hello", n)
+}
--- main.go
+++ main.go
@@ -1,28 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
-
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-	return initials[0], "_"
-}
+import "fmt"
 
 func main() {
-	fn1, sn1 := getInitials("lucy martin")
-	fn2, sn2 := getInitials("timber")
+	sayHi("Nine")
 
-	fmt.Println(fn1, sn1)
-	fmt.Println(fn2, sn2)
+	for _, v := range points {
+		fmt.Println(v)
+	}
 }

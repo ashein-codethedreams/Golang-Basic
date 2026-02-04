d391b6eb8c088b51ed7292aade0b4e15e5e5ebd8
ashein-codethedreams <heroglasses155@gmail.com>
2026-02-04 09:15:14 +0630

    Pass by value

--- main.go
+++ main.go
@@ -2,30 +2,26 @@ package main
 
 import "fmt"
 
+func updateName(x string) string {
+	x = "Code the dreams"
+	return x
+}
+
+func updateMenu(y map[string]float32) {
+	y["coffee"] = 4.6
+}
+
 func main() {
-	menu := map[string]float64{ //key-string & value-float
-		"pie":   34.6,
-		"soda":  12.5,
-		"pizza": 41,
+	// group A types -> strings, ints, bools, floats, arrays, structs
+	name := "Nine"
+	name = updateName(name)
+	fmt.Println(name)
+
+	// group B types -> slices, maps, functions
+	menu := map[string]float32{
+		"lipo":  3.2,
+		"shark": 2.7,
 	}
+	updateMenu(menu) //merge menu
 	fmt.Println(menu)
-	fmt.Println(menu["pizza"])
-
-	//lopping maps
-	for k, v := range menu {
-		fmt.Println(k, "-", v)
-	}
-
-	//int as key type
-	postalCodes := map[int]string{
-		11111: "Sanchaung",
-		11051: "Hlaing",
-		11421: "Dagon",
-	}
-	fmt.Println(postalCodes)
-
-	//update values
-	postalCodes[11421] = "Dagon Myo Thit"
-	fmt.Println("updated -", postalCodes)
-
 }

36c9f60824baef4d916e1bc3c124333da49db0d9
ashein-codethedreams <heroglasses155@gmail.com>
2026-02-04 09:49:31 +0630

    structs & custom types

--- main.go
+++ main.go
@@ -2,21 +2,7 @@ package main
 
 import "fmt"
 
-func updateName(n *string) {
-	*n = "luffy"
-}
 func main() {
-	name := "zed"
-
-	// & gets the memory address of the value (pointer)
-	// * gets the value at the specified memory address
-	m := &name //pointer
-
-	//fmt.Println("memory address:", m) //0x...
-	//fmt.Println("value at memory address:", *m) //zed
-
-	fmt.Println(name)
-	updateName(m)
-	fmt.Println(name)
-
+	mybill := newBill("mario's bill")
+	fmt.Println(mybill)
 }

0eeefa0672b0747b0a609720398dc294355ac409
ashein-codethedreams <heroglasses155@gmail.com>
2026-02-04 08:53:28 +0630

    maps

--- main.go
+++ main.go
@@ -3,9 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	sayHi("Nine")
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{ //key-string & value-float
+		"pie":   34.6,
+		"soda":  12.5,
+		"pizza": 41,
 	}
+	fmt.Println(menu)
+	fmt.Println(menu["pizza"])
+
+	//lopping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	//int as key type
+	postalCodes := map[int]string{
+		11111: "Sanchaung",
+		11051: "Hlaing",
+		11421: "Dagon",
+	}
+	fmt.Println(postalCodes)
+
+	//update values
+	postalCodes[11421] = "Dagon Myo Thit"
+	fmt.Println("updated -", postalCodes)
+
 }

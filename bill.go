e204b27c56d4a452484f3e1c7fad99acd8384f49
ashein-codethedreams <heroglasses155@gmail.com>
2026-02-05 08:31:33 +0630

    Receiver functions

--- bill.go
+++ bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -10,8 +12,24 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{},
+		items: map[string]float64{"pie": 25.3, "coffee": 12.65, "ham": 23},
 		tip:   0,
 	}
 	return b
+}
+
+//format the bill
+func (b bill) format() string {
+	fs := "Bill breakdown: \n"
+	var total float64 = 0
+
+	//list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-20v ...$%v \n", k+":", v)
+		total += v
+	}
+
+	//total
+	fs += fmt.Sprintf("%-20v ...$%0.2f", "total:", total)
+	return fs
 }

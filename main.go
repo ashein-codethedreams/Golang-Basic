cba48aafb6e3a118977a954dfc89736ee94df791
ashein-codethedreams <heroglasses155@gmail.com>
2026-02-07 22:30:44 +0630

    switch statement

--- main.go
+++ main.go
@@ -30,7 +30,20 @@ func promptOptions(b bill) {
 	reader := bufio.NewReader(os.Stdin)
 
 	opt,_ :=getInput("Choose option (a - add item, s - save the bill, t - add tip) :",reader)
-	fmt.Println(opt)
+	switch opt{
+	case "a":
+		name,_:= getInput("Enter the item's name: ",reader)
+		price,_:= getInput("Enter the item's price: ",reader)
+		fmt.Println(name,price)
+	case "s":
+		fmt.Println("u chose s")
+	case "t":
+		tip,_:= getInput("Enter the tip($): ",reader)
+		fmt.Println(tip)
+	default:
+		fmt.Println("invalid option")
+		promptOptions(b)
+	}
 }
 func main() {
 	mybill := createBill()
